1b8a74e60e9d052097ed7b5df3a0c7773ae34d73
The-morning-star23 <kumarshubh263@gmail.com>
2025-07-10 20:08:46 +0530

    corrected App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import TopBar from "./components/TopBar";
 import Toolbar from "./components/Toolbar";
-import SheetHeader from "./components/SheetHeadr";
+import SheetHeader from "./components/SheetHeader"; // typo fixed
 import Table from "./components/Table";
 import Pagination from "./components/Pagination";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50 text-sm overflow-x-auto">
-      <TopBar />           {/* top navbar with icons + avatar */}
+    <div className="relative min-h-screen bg-gray-50 text-sm overflow-x-auto">
+      {/* Top nav bar */}
+      <TopBar />
+
+      {/* Toolbar just below TopBar */}
       <Toolbar />
-      <SheetHeader />      {/* sheet title + tab + buttons */}
-      <Table />            {/* spreadsheet */}
-      <Pagination />       {/* 2 / 2 at bottom */}
+
+      {/* Q3 Tab and Right Icons, fixed below toolbar */}
+      <SheetHeader />
+
+      {/* Spreadsheet Table */}
+      <div className="relative z-10"> {/* ensures SheetHeader overlaps if needed */}
+        <Table />
+        <Pagination />
+      </div>
     </div>
   );
 }
